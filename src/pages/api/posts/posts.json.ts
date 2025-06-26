baba65d84f2ff0db9960c693890ca79d0a91fd30
Renan Salazar <renan@interconexo.com>
2025-06-25 22:32:29 -0400

    Instalado SSR Adapter Node

--- src/pages/api/posts/posts.json.ts
+++ src/pages/api/posts/posts.json.ts
@@ -3,38 +3,26 @@
 import type { APIRoute } from 'astro'
 import { getCollection } from 'astro:content';
 
-export const GET: APIRoute = async ({ params }) => {
+export const prerender = false;
+export const GET: APIRoute = async ({ params, request }) => {
 
- const blogPosts = await getCollection('blog');
 
-  const posts = blogPosts.map(post => ({
-    title: post.data.title,
-    description: post.data.description,
-    pubDate: post.data.pubDate,
-    updatedDate: post.data.updatedDate,
-    heroImage: post.data.heroImage
-  }));
+  const blogPosts = await getCollection('blog');
+  
+  console.log(request);
 
-  return new Response(JSON.stringify(posts), {
+  // const posts = blogPosts.map(post => ({
+  //   title: post.data.title,
+  //   description: post.data.description,
+  //   pubDate: post.data.pubDate,
+  //   updatedDate: post.data.updatedDate,
+  //   heroImage: post.data.heroImage
+  // }));
+
+  return new Response(JSON.stringify(blogPosts), {
      status: 200,
      headers: {
        'Content-Type': 'application/json'
      }
   })
-
-  // const { person } = params
-
-  // // Simulate fetching person data
-  // const personData = {
-  //   name: person,
-  //   age: Math.floor(Math.random() * 100),
-  //   occupation: 'Developer'
-  // }
-
-  // return new Response(JSON.stringify(personData), {
-  //   status: 200,
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   }
-  // })
 }
